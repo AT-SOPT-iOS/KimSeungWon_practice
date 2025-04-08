96785aa029db5f28c1dc82404098039a71844f91
Won <andrew112517@naver.com>
2025-04-08 14:31:09 +0900

    [setting] 프로젝트 세팅

---
+++ CarrotMarket/CarrotMarket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarrotMarket
+//
+//  Created by 김승원 on 4/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
