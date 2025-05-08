f00aecb79869fa9938408f35d3c6fdcf5e8fffad
Won <andrew112517@naver.com>
2025-05-08 18:03:20 +0900

    [feat] NetworkProvider

---
+++ CarrotMarket/CarrotMarket/Presentation/Week4/Network/Base/NetworkProvider.swift
@@ -0,0 +1,71 @@
+//
+//  NetworkProvider.swift
+//  CarrotMarket
+//
+//  Created by 김승원 on 5/8/25.
+//
+
+import Foundation
+
+struct NetworkProvider<Target: TargetType> {
+    func request(_ target: Target) throws -> URLRequest {
+        
+        // MARK: - URL Path
+        
+        let path = target.baseURL + target.path
+        guard var urlComponents = URLComponents(string: path) else {
+            throw NetworkError.invalidURLComponents
+        }
+        
+        // MARK: - Task
+        
+        var body: Data?
+        var url: URL?
+        
+        let task = target.task
+        switch task {
+        case .requestPlain:
+            url = urlComponents.url
+            
+        case .requestParameters(let parameters, let encoding):
+            switch encoding {
+            case .queryString:
+                let queryItemArray = parameters.map {
+                    URLQueryItem(name: $0.key, value: $0.value as? String)
+                }
+                urlComponents.queryItems = queryItemArray
+                url = urlComponents.url
+            }
+            
+        case .requestJSONEncodable(encodable: let encodable):
+            url = urlComponents.url
+            do {
+                let JSONEncoder = JSONEncoder()
+                let requestBody = try JSONEncoder.encode(encodable)
+                body = requestBody
+            } catch {
+                print(error)
+            }
+        }
+        
+        guard let url else {
+            throw NetworkError.invalidURLString
+        }
+        
+        // MARK: - HTTP Method
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = target.method.rawValue
+        request.httpBody = body
+        
+        // MARK: - Header
+        
+        if let headerField = target.headers {
+            _ = headerField.map { (key, value) in
+                request.addValue(value, forHTTPHeaderField: key)
+            }
+        }
+        
+        return request
+    }
+}
