2efc55f9b55b61bdebbfd8aea5583235ff637e27
Won <andrew112517@naver.com>
2025-05-17 12:53:28 +0900

    [setting] SwiftUI 프로젝트 세팅

---
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by 김승원 on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
